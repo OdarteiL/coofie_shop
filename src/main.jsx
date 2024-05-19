ddef2a33abd64d9264fbb9f278a9cafde0ba5d84
Odartei Lamptey <odarteilamptey.ml@gmail.com>
2024-05-19 21:05:26 +0100

    created a new config file

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import AppRouter from './router/AppRouter.jsx'
+import config from '../config.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
