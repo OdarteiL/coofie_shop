6612cf3f02c0afac805b4ce7475432e0a1909259
Odartei Lamptey <odarteilamptey.ml@gmail.com>
2024-05-19 22:11:26 +0100

    added some pages and navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import { Outlet } from 'react-router-dom'
-import './App.css'
+import { Outlet } from "react-router-dom";
+import "./App.css";
+import Navbar from "./components/Navbar";
 
 function App() {
-
-
   return (
     <>
-     <Outlet />
+      <Navbar />
+      <Outlet />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,11 +1,15 @@
 import { createBrowserRouter, createRoutesFromElements, Route } from "react-router-dom";
 import App from "../App";
 import Homepage from "../pages/Homepage";
+import SignupPage from "../pages/SignupPage";
+import SigninPage from "../pages/SigninPage";
 
 const AppRouter = createBrowserRouter(
     createRoutesFromElements(
         <Route path="/" element={<App />}>
             <Route index element={<Homepage />} />
+            <Route path="/signup" element={<SignupPage />} />
+            <Route path="/signIn" element={<SigninPage />} />
         </Route>
     )
 ) 
